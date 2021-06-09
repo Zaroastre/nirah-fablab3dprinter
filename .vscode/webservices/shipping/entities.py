a35e80bc73295d631d7c84ac8cde7683134895c7
NicolasMetivier-at-162491939266 <metivier.nicolas@engie.com>
2021-06-09 11:08:35 +0200

    Remove copy files

--- .vscode/webservices/shipping/entities.py
+++
@@ -1,6 +0,0 @@
-class ShippingCard:
-    def __init__(self):
-        self.identifier: int = -1;
-        self.owner: int = None;
-        self.creation_date: int = -1;
-        self.products: list = [];
